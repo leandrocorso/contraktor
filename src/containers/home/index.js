52d4da5112dbf8c5a71ce0317780e9687c88f6db
Leandro Corso <contato@leandrocorso.com.br>
2019-08-13 00:04:51 -0300

    a simple homepage

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Button } from '@material-ui/core';
+
+const btnContracts = React.forwardRef((props, ref) => (
+  <NavLink activeClassName="active" innerRef={ref} to="/contratos" {...props} />
+));
+
+const btnParts = React.forwardRef((props, ref) => (
+  <NavLink activeClassName="active" innerRef={ref} to="/partes" {...props} />
+));
 
 function Home() {
-  return <h2>Início</h2>;
+  return (
+    <>
+      <h1>Início</h1>
+
+      <p>Selecione uma opção</p>
+      <p>
+        <Button component={btnContracts} color="primary" size="large" variant="contained">
+          Contratos
+        </Button>
+      </p>
+
+      <p>
+        <Button component={btnParts} color="primary" size="large" variant="contained">
+          Partes
+        </Button>
+      </p>
+    </>
+  );
 }
 
 export default Home;
